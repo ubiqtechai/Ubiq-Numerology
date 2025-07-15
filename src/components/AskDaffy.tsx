464a8dcb386b818b9172af685d34fbf27a6b867b
ubiqtechai <rb@aumirah.com>
2025-07-15 14:02:37 +0530

    Updated AskDaffy.tsx

--- src/components/AskDaffy.tsx
+++ src/components/AskDaffy.tsx
@@ -284,7 +284,6 @@ const AskDaffy = () => {
                       <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 016 0v6a3 3 0 01-3 3z"></path>
 
-                        
 
                       </svg>
                     </div>

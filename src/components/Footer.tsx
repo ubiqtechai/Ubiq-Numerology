72ec1470b84db73eedcae2600ee255bda50ccb1e
ubiqtechai <rb@aumirah.com>
2025-07-16 13:56:55 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -74,7 +74,7 @@ const Footer = () => {
           <div className="md:col-span-1">
             <div className="flex items-center gap-3 mb-4">
               
-              {/* <div className="w-12 h-12 rounded-full bg-gradient-to-r from-saffron to-gold flex items-center justify-center"> */}
+              <div className="w-12 h-12 rounded-full bg-gradient-to-r from-saffron to-gold flex items-center justify-center">
                 {/* <span className="text-white font-bold text-xl">U</span> */}
                 <span className="text-white font-bold text-lg">
                   <img

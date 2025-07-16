6e470b32d69e1b38577ac88ab8a60a32cf3f45c8
ubiqtechai <rb@aumirah.com>
2025-07-16 13:59:01 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -79,7 +79,7 @@ const Footer = () => {
                   <img
                     src="https://ubiqtech.ai/ubiq-logo.svg"
                     alt="U"
-                    className="w-full h-full object-contain
+                    className="w-full h-full object-contain"
                     
                   />
                 </span>
